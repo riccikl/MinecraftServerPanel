a94f85022752cb8ebba868e521c3f0ccc1edebb3
riccikl <49199340+riccikl@users.noreply.github.com>
2021-10-31 23:22:16 +0100

    initial layout (#2)

--- MinecraftServerPanel/Pages/Index.cshtml.cs
+++ MinecraftServerPanel/Pages/Index.cshtml.cs
@@ -10,15 +10,11 @@ namespace MinecraftServerPanel.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
+        public bool ServerStatus { get; set; }
+        
         public void OnGet()
         {
+            ServerStatus = true;
         }
     }
 }
